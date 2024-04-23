753aec9ed3e7daead3a93ad8a58a7dc8fc57f06c
sko <sko@synapsia.fr>
2024-04-23 18:07:58 +0200

    rm css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const items = await db.query.test.findMany()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
         {items.map((item) => (
           <div key={item.id}><p>user connected : {item.name}</p></div>
         ))}
